40089606e12ab8879d6f53fa64c141931765e0b2
enoch <henokt828@gmail.com>
2025-07-22 09:53:32 +0300

    feat: add DisposableAssetRequest class for handling disposable asset requests

---
+++ Inventory-Information/src/main/java/com/MMS/Inventory_Information/dto/request/DisposableAssetRequest.java
@@ -0,0 +1,28 @@
+package com.MMS.Inventory_Information.dto.request;
+
+import com.MMS.Inventory_Information.enums.DisposableType;
+import com.MMS.Inventory_Information.enums.DisposalStatus;
+import com.MMS.Inventory_Information.model.DisposalCollection.DisposableFixedAssetDetail;
+import lombok.Data;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+public class DisposableAssetRequest {
+      private UUID tenantId;
+      private String drNo;
+      private UUID itemId;
+      private UUID storeId;
+      private UUID departmentId;
+      private UUID processedById;
+      private DisposableType disposableType;
+      private LocalDate requisitionDate;
+      private DisposalStatus disposalStatus;
+      private String processedBy;
+      private LocalDate processedOn;
+
+      private List<DisposableFixedAssetDetail> disposableFixedAssetDetails;
+
+}
