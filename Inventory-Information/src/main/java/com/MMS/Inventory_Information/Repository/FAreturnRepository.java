b65358af56357183541a3d3e6b723d613b70f221
enoch <henokt828@gmail.com>
2025-07-22 09:26:17 +0300

    fix: delete the FAreturnRepository

--- Inventory-Information/src/main/java/com/MMS/Inventory_Information/Repository/FAreturnRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MMS.Inventory_Information.Repository;
-
-import com.MMS.Inventory_Information.model.FixedAssetReturn.FixedAssetReturn;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface FAreturnRepository extends JpaRepository<FixedAssetReturn, UUID> {
-}
